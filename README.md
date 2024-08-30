1ffa385ab0f079892721f61c228f54015290ad6b
hrkmtsmt <hrkmtsmt@icloud.com>
2024-08-31 00:04:58 +0900

    First commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+# ✊ Hands-on Test-Driven Development
+
+- このリポジトリは書籍『テスト駆動開発』を実践するためのリポジトリです。
+- 書籍の言語はJavaですが、ここではTypeScriptを使います。
+
+## 💯 テスト
+
+```bash
+bun test
+```
---
+++ src/main.test.ts
@@ -0,0 +1,8 @@
+import { describe, test, expect } from 'bun:test';
+import { main } from './main';
+
+describe('function main', () => {
+	test('return string message.', () => {
+		expect(main()).toBe('Hello Test-Driven Development!');
+	});
+});
